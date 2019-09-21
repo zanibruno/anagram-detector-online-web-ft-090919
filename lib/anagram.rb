3a4280a3c0e23de0f0de7d037d6e8790d928257e
brunozanin <corporate.zanin@gmail.com>
2019-09-21 17:48:29 +0100

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+# Your code goes here
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(string)
+    string.find_all do |word|
+      if word.split("").sort == self.word.split("").sort
+        word
+      end
+    end
+  end
+
+end 
